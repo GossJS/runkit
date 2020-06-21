c899911daade40015e862eac7032ce0897e42c50
Ilya Goss <goss@corp.ifmo.ru>
2020-06-22 02:12:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const x = require('express');
+const app = x();
+app
+.get('/', (req, res) => res
+  .status(200)
+  .set({"Content-Type":"application/json; charset=utf-8"})
+  .end('{"Hello":"world!"}')
+)
+.listen(4321);
